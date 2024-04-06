758491c3aa328dbcc0b67fe3231487e0765fb7d6
yubaoquan <yuyuyubq@163.com>
2024-04-06 18:13:12 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,4 @@
+# dutor
+
+Recursive detecting duplicate files in a folder.
+管理文件夹中内容相同的文件. 这是本工具的前端工程.
---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+import './assets/style.css';
+
+// import './assets/base.css';
+
+import App from './App.vue';
+import router from './router/index.ts';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(vuetify);
+
+app.mount('#app');
